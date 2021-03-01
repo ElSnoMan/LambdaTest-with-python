5521c6d9b4ee3000864d4f04ea12b0fcb0b85fc2
Carlos <ckcarlosdesign@gmail.com>
2021-03-01 13:23:14 -0700

    Refactor logic to TodoPage and page fixture

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,4 +1,26 @@
+import pytest
 from pylenium.driver import Pylenium
+from pylenium.element import Element, Elements
+
+
+class TodoPage:
+    def __init__(self, py: Pylenium):
+        self.py = py
+
+    def goto(self) -> 'TodoPage':
+        self.py.visit('https://lambdatest.github.io/sample-todo-app/')
+        return self
+
+    def get_todo_by_name(self, name: str) -> Element:
+        return self.py.getx(f"//*[text()='{name}']").parent()
+
+    def get_all_todos(self) -> Elements:
+        return self.py.find("li[ng-repeat*='todo']")
+
+
+@pytest.fixture
+def page(py: Pylenium):
+    return TodoPage(py).goto()
 
 
 def test_check_first_item(py: Pylenium):
